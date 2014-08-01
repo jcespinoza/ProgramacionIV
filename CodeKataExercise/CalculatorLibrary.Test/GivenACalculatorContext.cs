bfd7d293c0f2e945a7a30b38edef3f402e4a647b
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-07-31 23:45:41 -0600

    New Homework -- StringCalculator
    From osherome.com, due on monday

---
+++ CodeKataExercise/CalculatorLibrary.Test/GivenACalculatorContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorLibrary;
+using Machine.Specifications;
+
+namespace CalculatorLibrary.Test
+{
+    public class GivenACalculatorContext
+    {
+        public static StringCalculator strCalc;
+        Establish context = () =>
+        {
+            strCalc = new StringCalculator();
+        };
+    }
+}
---
+++ CodeKataExercise/CalculatorLibrary.Test/whenInputIsEmptyString.cs
@@ -0,0 +1,18 @@
+﻿using Machine.Specifications;
+
+namespace CalculatorLibrary.Test
+{
+    public class whenInputIsEmptyString: GivenACalculatorContext
+    {
+        private static int result;
+        private Because of = () =>
+        {
+            result = strCalc.Sum("");
+        };
+
+        private It shouldReturnZero = () =>
+        {
+            result.ShouldEqual(0);
+        };
+    }
+}
