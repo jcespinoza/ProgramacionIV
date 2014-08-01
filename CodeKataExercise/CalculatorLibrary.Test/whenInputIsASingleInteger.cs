50246f6b2439979bb193d79316e73c39972745b1
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-08-01 00:06:05 -0600

    Handles an integer

---
+++ CodeKataExercise/CalculatorLibrary.Test/whenInputIsASingleInteger.cs
@@ -0,0 +1,18 @@
+﻿using Machine.Specifications;
+
+namespace CalculatorLibrary.Test
+{
+    public class whenInputIsASingleInteger: GivenACalculatorContext
+    {
+        private static int input = 98;
+        private Because of = () =>
+        {
+            functionResult = strCalc.Sum(input.ToString());
+        };
+
+        private It shouldReturnTheInput = () =>
+        {
+            functionResult.ShouldEqual(input);
+        };
+    }
+}
